2c1f3e1a1dfce34101f460f060bf9f60112cdd65
MahmdulHasan <mahmudul92hasan@gmail.com>
2023-10-29 20:10:42 +0600

    run the migration to create database tables

---
+++ S3InnovateTask2/src/Libraries/App.Data/Migrations/20231028143211_InitialCreate.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Building",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Building", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DataField",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataField", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Object",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Object", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reading",
+                columns: table => new
+                {
+                    BuildingId = table.Column<int>(type: "int", nullable: false),
+                    ObjectId = table.Column<int>(type: "int", nullable: false),
+                    DataFieldId = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reading", x => new { x.BuildingId, x.ObjectId, x.DataFieldId });
+                    table.ForeignKey(
+                        name: "FK_Reading_Building_BuildingId",
+                        column: x => x.BuildingId,
+                        principalTable: "Building",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reading_DataField_DataFieldId",
+                        column: x => x.DataFieldId,
+                        principalTable: "DataField",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reading_Object_ObjectId",
+                        column: x => x.ObjectId,
+                        principalTable: "Object",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reading_DataFieldId",
+                table: "Reading",
+                column: "DataFieldId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reading_ObjectId",
+                table: "Reading",
+                column: "ObjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reading");
+
+            migrationBuilder.DropTable(
+                name: "Building");
+
+            migrationBuilder.DropTable(
+                name: "DataField");
+
+            migrationBuilder.DropTable(
+                name: "Object");
+        }
+    }
+}
---
+++ S3InnovateTask2/src/Libraries/App.Data/Migrations/20231029140116_AddSeedData.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Data.Migrations
+{
+    public partial class AddSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Building",
+                columns: new[] { "Id", "Location", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Location 1", "Building 1" },
+                    { 2, "Location 2", "Building 2" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "DataField",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "DataField 1" },
+                    { 2, "DataField 2" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Object",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Object 1" },
+                    { 2, "Object 2" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Building",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Building",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DataField",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DataField",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Object",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Object",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
